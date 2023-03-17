a35333712d3f14b60f3900ca431998e5317536e0
Rayanemadne <r.madoune@gmail.com>
2023-03-16 18:50:56 -0700

    script 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,8 @@
 
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char qut[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1, quote, 59);
+	write(1, qut, 59);
 	return (1);
 }
