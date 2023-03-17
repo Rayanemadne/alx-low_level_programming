8a4581af383ec1285004fcf907a6e060d48c4856
Rayanemadne <r.madoune@gmail.com>
2023-03-17 13:05:02 -0700

    script 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - hexadecimalos
+ *
+ * Description: printing hexadecimal numbers
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57;i++)
+	{
+		putchar(n);
+	}
+	for (n = 97; n <= 102;i++)
+	{
+		putchar(n);
+	}
+		putchar('\n');
+		return (0);
+}
