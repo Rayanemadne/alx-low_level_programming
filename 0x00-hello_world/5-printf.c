0963abf8cc633edd98cbe3e3a42ab7d7625ad0d1
Rayanemadne <r.madoune@gmail.com>
2023-03-16 18:00:16 -0700

    script 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - printer 
+ * main - printer
  *
  * Description : print with the function printf
  *
- * return : Always 0 (Success)
+ * Return : Always 0 (Success)
 */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return 0;
 }
