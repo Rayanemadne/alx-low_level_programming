80c605e55268c21cc34e5ff3e45c2d903115d748
Rayanemadne <r.madoune@gmail.com>
2023-03-17 12:30:34 -0700

    script 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - 0 to 9 v2
+ *
+ * Description: print number from 0 to 9
+ *
+ * Return:0 (Success)
+*/
+
+int main(void)
+{
+        int n;
+
+        for (n = 0; n < 10; n++)
+        {
+                putchar(n);
+        }
+        putchar('\n');
+        return (0);
+}
