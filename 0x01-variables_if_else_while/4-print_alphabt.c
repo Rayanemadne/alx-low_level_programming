0d0782f15595d98c9fe5e90c4b3ab32d84074cee
Rayanemadne <r.madoune@gmail.com>
2023-03-17 12:11:30 -0700

    script 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - alphab t where is the e ? and the q
+ *
+ * Description: this program print all alphabet except e and q
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int al;
+
+	for (al = 'a'; al <= 'z'; al++)
+	{
+		if (al != 'e' || al != 'q')
+		{
+			putchar(al);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
