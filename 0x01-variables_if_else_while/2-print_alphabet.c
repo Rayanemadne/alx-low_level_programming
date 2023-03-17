34a1a156fd7152058042afc1b1f964d5d96bb6b0
Rayanemadne <r.madoune@gmail.com>
2023-03-17 10:15:38 -0700

    script 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Alphabet kido
+ *
+ * Description: print alphabet 
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int al;
+	for (al = 'a'; al <= 'z'; al++)
+	{
+		putchar(al);
+	}
+	return (0);
+}
