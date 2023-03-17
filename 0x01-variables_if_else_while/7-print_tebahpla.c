ad69779f68922195a8932e9c9b939f81f07a80f2
Rayanemadne <r.madoune@gmail.com>
2023-03-17 12:50:01 -0700

    script 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Alphabet kido
+ *
+ * Description: print alphabet
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int al;
+
+	for (al = 'z'; al <= 'a'; al--)
+	{
+		putchar(al);
+	}
+	putchar('\n');
+	return (0);
+}
