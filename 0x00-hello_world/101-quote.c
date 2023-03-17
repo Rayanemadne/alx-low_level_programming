39b673bdd7bf9ba4520a0646dad6d1e3545d52b1
Rayanemadne <r.madoune@gmail.com>
2023-03-16 18:48:06 -0700

    script101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include<unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print a quote using write function
+ *
+ * Return: an error 1.
+*/
+
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quote, 59);
+	return (1);
+}
