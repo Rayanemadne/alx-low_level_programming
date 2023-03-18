96be0c13467583e50c3815674da1df3bff020839
Rayanemadne <r.madoune@gmail.com>
2023-03-17 19:47:10 -0700

    script 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - combinator
+ *
+ * Description: print combination
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int i, j;
+
+	i = 0;
+
+	while (i <= 9)
+	{
+		j = i;
+		while (j <= 9)
+		{
+			if (i != j)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(',');
+				putchar('');
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
