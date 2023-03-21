1cd9502f086d4160ea2b9943eaa2391d2fe14adb
Rayanemadne <r.madoune@gmail.com>
2023-03-21 13:29:37 -0700

    script 2 modif

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,14 @@
 
 void print_alphabet_x10(void)
 {
-	int i,al;
+	int i, al;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (al = 97; al < 123; al++)
 		{
 			putchar(al);
 		}
-		putchar('\n');	
+		putchar('\n');
 	}
 }
