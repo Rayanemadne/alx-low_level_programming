a4ad45abd4334dd788c1d4b5faa519883141092a
Rayanemadne <r.madoune@gmail.com>
2023-03-21 17:59:07 -0700

    script 102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main- fibonnacci
+ *
+ * Description: fibonnaci formula cherche it on google
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	int f1 = 0, f2 = 1, sum;
+
+	for (i = 0 ; i < 50; i++)
+	{
+		sum = f1 + f2;
+		printf("%d", sum);
+
+		f1 = f2
+		f2 = sum
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}
