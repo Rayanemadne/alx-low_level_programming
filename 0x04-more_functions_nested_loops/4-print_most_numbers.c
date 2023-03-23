013cbacc3267bf28ea75c997d15dd1cdef290436
Rayanemadne <r.madoune@gmail.com>
2023-03-23 08:47:19 -0700

    script 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print 0 - 9 exept 2 and 4
+ *
+ *
+ * Return: 0 (Success)
+*/
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; ++i)
+	{
+		if (i != 2 || i != 4)
+			_putchar(num + 48);
+	}
+	_putchar('\n');
+}
