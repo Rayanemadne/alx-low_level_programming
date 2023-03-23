282f925ce72428f918441a01115f5ae4ddfb8d95
Rayanemadne <r.madoune@gmail.com>
2023-03-23 08:31:02 -0700

    script 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,18 +2,18 @@
 
 /**
  * print_numbers - print 0 - 9
- *             only using _putchar twice
  *
- * Return: Always 0 (Success)
+ *
+ * Return: 0 (Success)
 */
 
 void print_numbers(void)
 {
-	int i;
+	int num = 0;
 
-	for (i = 0; i >= 0 && i <= 9; i++)
-	{
-		_putchar(i + 48);
-	}
+	do {
+		_putchar(num + 48);
+		num++;
+	} while (num >= 0 && num <= 9);
 	_putchar('\n');
 }
