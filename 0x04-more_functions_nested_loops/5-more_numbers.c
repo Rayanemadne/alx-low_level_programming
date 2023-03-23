4b4056f3f4d125d489b9083442a7cc5462309a5e
Rayanemadne <r.madoune@gmail.com>
2023-03-23 09:21:12 -0700

    script 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,20 +9,22 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, c;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
+			c = j;
 			if (j > 9)
 			{
 				_putchar(1 + 48);
-				_putchar((j % 10) + 48);
+				c = c % 10;
+				_putchar(c + 48);
 			}
 			else
 			{
-				_putchar(j + 48);
+				_putchar(c + 48);
 			}
 		}
 		_putchar('\n');
