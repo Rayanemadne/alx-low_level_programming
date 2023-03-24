a3a866194790dcecdffe39671589b8fe0ea6167f
Rayanemadne <r.madoune@gmail.com>
2023-03-24 08:12:14 -0700

    script 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,20 +11,16 @@ void print_diagonal(int n)
 {
 	int i, sp;
 
-	if (n > 0)
+	if (n <= 0)
+		_putchar('\n');
+	else
 	{
 		for (i = 1; i <= n; i++)
 		{
 			for (sp = 1; sp <= i; sp++)
-			{
-				putchar(' ');
-			}
+				_putchar(' ');
 			_putchar(92);
 			_putchar('\n');
 		}
-	}
-	else
-	{
-		_putchar('\n');
 	}
 }
