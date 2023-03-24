00378d784b660286ca718b80d932d29bde23a76f
Rayanemadne <r.madoune@gmail.com>
2023-03-24 07:06:31 -0700

    script 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - print a straight line
+ *
+ * @n: input
+ *
+*/
+
+void print_line(int n)
+{
+	int ln;
+
+	if (n > 0)
+	{
+		for (ln = 0; ln < n; ln++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
