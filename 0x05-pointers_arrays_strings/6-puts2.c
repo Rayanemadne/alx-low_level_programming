8098f45426a3e205fbea9812dd3416c335eade1b
Rayanemadne <r.madoune@gmail.com>
2023-03-27 14:30:06 +0200

    script 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - jumping by one
+ *
+ *
+ * @str: intput
+ *
+ * Return: Nothing
+ */
+
+void puts2(char *str)
+{
+	int i, len;
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+
+	for (i = 0; i < len; i = i + 2)
+	{
+	putchar(*(str + i));
+	}
+}
