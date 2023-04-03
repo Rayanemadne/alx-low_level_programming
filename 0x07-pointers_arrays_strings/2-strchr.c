8ccff1bfbf9793aa7249d5381b77081e67e71836
Rayanemadne <r.madoune@gmail.com>
2023-04-03 15:53:06 +0200

    script 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - enemy located
+ *
+ * @s: input for the memory that we want
+ * @c: input that we want to locate
+ *
+ * Return: return s
+*/
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i, n;
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return ('\0');
+}
