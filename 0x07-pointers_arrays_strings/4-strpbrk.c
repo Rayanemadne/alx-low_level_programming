6c0c2cdb76b17876f4d1416b840bf4fe8c9714a9
Rayanemadne <r.madoune@gmail.com>
2023-04-03 19:53:52 +0200

    script 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes.
+ *
+ * @s: input of the string
+ * @accept: input of the caracteres
+ *
+ * Return: return a pointer to the byte in s
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+	char checker;
+
+	if (s == NULL || accept == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+			return (s + i);
+			}
+		}
+	}
+}
