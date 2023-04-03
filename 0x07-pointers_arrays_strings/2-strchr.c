9a6b189d3b60f651f277cce09f84babe11987515
Rayanemadne <r.madoune@gmail.com>
2023-04-03 16:02:15 +0200

    script 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,13 +6,11 @@
  * @s: input for the memory that we want
  * @c: input that we want to locate
  *
- * Return: return s
+ * Return: return null if not found
 */
 
 char *_strchr(char *s, char c)
 {
-	unsigned int i, n;
-
 	while (*s != '\0')
 	{
 		if (*s == c)
