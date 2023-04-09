04fa07208e3e6482bcc4e5250fedecad97fe777d
Rayanemadne <r.madoune@gmail.com>
2023-04-10 01:07:12 +0200

    script 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/*RAYANE - MAD*/
+
+/**
+ * main -  multiplies two numbers
+ *
+ * Description: print the result of the multiplication
+ * @argc: number of arguments
+ * @argv: array that contain all the argument
+ *
+ * Return: 0 (Success) and 1 if not enough argument
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	int sum = atoi(argv[1]) * atoi(argv[2]);
+
+	printf("%d\n", sum);
+
+	return (0);
+}
