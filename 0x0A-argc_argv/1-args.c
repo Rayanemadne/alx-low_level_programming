eabf0048eb3cfbe2175c5edd14f27ac60ff1f20e
Rayanemadne <r.madoune@gmail.com>
2023-04-10 00:43:21 +0200

    script 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*RAYANE - MAD*/
+
+/**
+ * main -  number of arguments
+ *
+ * Description:  prints the number of arguments
+ * @argc: number of arguments
+ * @argv: array that contain all the argument
+ *
+ * Return: 0 (Success)
+*/
+
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
