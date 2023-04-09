590e3fde0c2d2379176e74f5df621737db2ace03
Rayanemadne <r.madoune@gmail.com>
2023-04-10 00:32:06 +0200

    script 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*RAYANE - MAD*/
+
+/**
+ * main - prints its name
+ *
+ * Description: print the name of the .exe
+ * @argc: number of arguments
+ * @argv: array that contain all the argument
+ *
+ * Return: 0 (Success)
+*/
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
