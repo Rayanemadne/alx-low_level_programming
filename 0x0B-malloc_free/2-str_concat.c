b7e27dcc4dc3d043cec46920340832c5e97a952d
Rayanemadne <r.madoune@gmail.com>
2023-04-11 18:03:08 +0200

    script 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,18 +19,27 @@ char *str_concat(char *s1, char *s2)
 	char *cc;
 	int l1, l2, i;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	l1 = strlen(s1);
 	l2 = strlen(s2);
 
-	cc = malloc(l1 + l2 + 1);
 
+	cc = malloc(l1 + l2 + 1);
+	if (cc == NULL)
+		return (NULL);
+	
 	for (i = 0; i < l1; i++)
 	{
 		cc[i] = s1[i];
 	}
+
 	for (i = 0; i < l2; i++)
 	{
-		cc[l1 + i] = s2[i];
+			cc[l1 + i] = s2[i];
 	}
 
 	return (cc);
