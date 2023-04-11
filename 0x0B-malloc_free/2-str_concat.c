ed172c0b1f6d4252013a8154ca7015b384649bf9
Rayanemadne <r.madoune@gmail.com>
2023-04-11 17:53:28 +0200

    script 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/*RAYANE - MAD*/
+
+/**
+ * str_concat - concatenator
+ *
+ * Description: concatenates two strings
+ *
+ * @s1: input of the first string
+ * @s2: input of the first string
+ *
+ * Return: a pointer
+*/
+
+
+char *str_concat(char *s1, char *s2)
+{
+	char *cc;
+	int l1, l2, i;
+
+	l1 = strlen(s1);
+	l2 = strlen(s2);
+
+	cc = malloc(l1 + l2 + 1);
+
+	for (i = 0; i < l1; i++)
+	{
+		cc[i] = s1[i];
+	}
+	for (i = 0; i < l2; i++)
+	{
+		cc[l1 + i] = s1[i];
+	}
+
+	return (cc);
+}
