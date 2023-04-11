5fbaf7dd673760dab85385bb2133774da7d5502c
Rayanemadne <r.madoune@gmail.com>
2023-04-11 17:33:11 +0200

    script 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/*RAYANE - MAD*/
+
+/**
+ * _strdup - copy string
+ *
+ * Description: allocated space in memory,
+ * which contains a copy of the string given
+ *
+ * @str: input of the string
+ *
+ * Return: a pointer
+*/
+
+
+char *_strdup(char *str)
+{
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	int len = strlen(str);
+	int i;
+	char *cp;
+
+	cp = malloc(len + 1);
+	if (cp == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+	{
+		cp[i] = str[i];
+	}
+
+	return (cp);
+}
