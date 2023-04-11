09a8e7543a24fa159468dd5f1cde14b0dcbc96cd
Rayanemadne <r.madoune@gmail.com>
2023-04-11 17:13:53 +0200

    script 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/*RAYANE - MAD*/
+
+/**
+ * create_array - creates an array of chars
+ *
+ * Description: creates an array and initializes it
+ *
+ * @size: the size of the array
+ * @c: the char to initialize with
+ *
+ * Return: the array
+*/
+
+
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	char a[] = malloc(size);
+
+	a[0] = *c;
+
+	char[size] = '\0';
+
+	return (a);
+}
