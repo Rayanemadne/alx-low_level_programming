37e2423813cd042d5bec985eb54d6c9952bb99d5
Rayanemadne <r.madoune@gmail.com>
2023-04-13 14:22:22 +0200

    script 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/*RAYANE - MAD*/
+
+/**
+ * array_range - allocate memory for an int array
+ *
+ * @min: input of min value
+ * @max: input of max value
+ *
+ * Return: pointer
+*/
+
+int *array_range(int min, int max)
+{
+	int *ar, diff, i, j;
+
+	/* min > max -> NULL */
+	if (min > max)
+		return (NULL);
+
+	/* calculat the diferance*/
+	diff = max - min;
+	/* alocat memory */
+
+	ar = malloc(sizeof(int) * diff + 1);
+
+	/* init the array with the range */
+
+	j = min;
+	for (i = 0; j <= max; i++)
+	{
+		ar[i] = j;
+		j++;
+	}
+
+	/* return array */
+	return (ar);
+}
