a0dc687da7adf82237a4c124ced6ec713b87fbdd
Rayanemadne <r.madoune@gmail.com>
2023-04-17 16:30:39 +0200

    header file 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structur of a new dog
+ *
+ * @name: the name
+ * @age: the age
+ * @owner: name of the owner
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
