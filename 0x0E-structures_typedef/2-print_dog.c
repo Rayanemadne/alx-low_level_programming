d23e497b4e9f250fc76cf6811421d1ee0d579e4f
Rayanemadne <r.madoune@gmail.com>
2023-04-17 16:53:13 +0200

    script 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+
+/*RAYANE - MAD*/
+
+/**
+ * print_dog - print structure
+ *
+ * @d: adress of the struct
+*/
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+
+		printf("Age: %f\n", d->age);
+
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
+
+	}
+
+}
