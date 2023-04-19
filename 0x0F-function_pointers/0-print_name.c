19a89fbc75195abc0dbcdd7319713b98ce77f78b
Rayanemadne <r.madoune@gmail.com>
2023-04-19 15:18:31 +0200

    script 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_name - print the name
+ * 
+ * @name: input of the name
+ * @f: input pointer of a function
+ * 
+ * Return: nada 
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f && name)
+		f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,2 @@
+leaning about function pointer 
+(a function that point to a another function that point to another function aaahhhh)
