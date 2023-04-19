7419625bb6f17839e458e386ce1e371cd0ed73da
Rayanemadne <r.madoune@gmail.com>
2023-04-19 15:19:49 +0200

    script 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,11 +2,11 @@
 
 /**
  * print_name - print the name
- * 
+ *
  * @name: input of the name
  * @f: input pointer of a function
- * 
- * Return: nada 
+ *
+ * Return: nada
 */
 
 void print_name(char *name, void (*f)(char *))
