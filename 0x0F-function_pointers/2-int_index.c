f376e1f7988963e56e2a10bcf4ba98fe11a909a2
Rayanemadne <r.madoune@gmail.com>
2023-04-19 19:02:41 +0200

    script 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,11 +16,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, j = 0;
 
-	if (size <= 0)
-		return (-1);
-
 	if (array != NULL && size > 0 && cmp != NULL)
 	{
+		if (size <= 0)
+			return (-1);
+
 		for (i = 0; i < size; i++)
 		{
 			if (cmp(array[i]) == 1)
