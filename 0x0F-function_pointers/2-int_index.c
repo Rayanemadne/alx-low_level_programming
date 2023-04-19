b3e772b8110be89e89032a6febef01f8439b053b
Rayanemadne <r.madoune@gmail.com>
2023-04-19 18:50:53 +0200

    script 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/*RAYANE - MAD*/
+
+/**
+ * int_index -  searches for an int
+ *
+ * @array: the array
+ * @size: the size of my...array
+ * @cmp: the function to compare values
+ *
+ * Return: index
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i, j = 0;
+
+	if (size <= 0)
+		return (-1);
+
+	if (array != NULL && size > 0 && cmp != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) == 0)
+			{
+				j = 1;
+				break;
+			}
+		}
+	}
+
+	if (i == size && j != 1)
+	{
+		return (-1);
+	}
+	else
+		return (i);
+}
