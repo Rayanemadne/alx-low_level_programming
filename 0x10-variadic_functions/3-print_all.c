4eaf1df7c7e44cb5a1fe420ec8eeb29d9233b919
Rayanemadne <r.madoune@gmail.com>
2023-04-20 22:25:34 +0200

    script 3 + cleaning

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,55 @@
+#include "variadic_functions.h"
+
+/*RAYANE - MAD*/
+
+/**
+ * print_all - print with diferent format
+ *
+ * @format: the type
+ *
+ * Return: nada
+*/
+
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *str, *sep = "";
+
+	va_list list;
+
+	va_start(list, format);
+
+	if (format)
+	{
+		while (format[i])
+		{
+			/*check the format*/
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(list, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(list, double));
+					break;
+				case 's':
+					str = va_arg(list, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			/*add the separation*/
+			sep = ", ";
+			i++;
+		}
+	}
+	printf("\n");
+	va_end(list);
+}
