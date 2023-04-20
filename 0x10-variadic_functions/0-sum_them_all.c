90994c27c2b5492a06992735347573aa3ac288d3
Rayanemadne <r.madoune@gmail.com>
2023-04-20 20:52:01 +0200

    script 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,8 +12,13 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	unsigned int i;
+	int sum = 0;
 	va_list args;
-	unsigned int i, sum = 0;
+
+	if (n == 0)
+		return (0);
+
 
 	va_start(args, n);
 
