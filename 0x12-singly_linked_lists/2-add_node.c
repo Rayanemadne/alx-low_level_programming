f8d0e9689dbfdee2a2d8405fc86b8077cfd12f0c
Rayanemadne <r.madoune@gmail.com>
2023-04-27 20:11:46 +0200

    script 2 update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,10 +12,24 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *newnode = malloc(sizeof(list_t));
+	list_t *newnode;
+	size_t strlen = 0;
+
+	/*set string length to 0 if its NULL*/
+	if (str == NULL)
+		strlen = 0;
+
+	/*count length of string*/
+	while (str[strlen] != '\0')
+		strlen++;
+
+	newnode = malloc(sizeof(list_t));
+	/*if it fail*/
+	if (newnode == NULL)
+		return (NULL);
 
 	newnode->str = strdup(str);
-	newnode->len = strlen(str);
+	newnode->len = strlen;
 	if (*head == NULL)
 		newnode->next = NULL;
 	else
