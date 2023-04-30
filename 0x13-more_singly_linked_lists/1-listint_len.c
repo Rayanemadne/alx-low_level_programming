7321499dbb3cf4a2030a9e678d9e7aac8cbe6853
Rayanemadne <r.madoune@gmail.com>
2023-04-30 18:19:15 +0200

    adjustment script 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -12,7 +12,7 @@
 
 size_t listint_len(const listint_t *h)
 {
-	size_t len;
+	size_t len = 1;
 
 	if (h == NULL)
 		return (0);
@@ -21,6 +21,7 @@ size_t listint_len(const listint_t *h)
 	{
 		if (h->n != '\0')
 			len++;
+
 		h = h->next;
 	}
 
