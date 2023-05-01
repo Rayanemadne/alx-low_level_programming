988f60eefb0bc92879f3748712c367d537d1a877
Rayanemadne <r.madoune@gmail.com>
2023-05-01 14:30:37 +0200

    script 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/*RAYANE - MAD*/
+
+/**
+ * pop_listint - deletes the head node
+ * @head: head of the list
+ *
+ * Return: the data
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int num;
+
+	if (!head || !*head)
+		return (0);
+
+	num = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+
+	return (num);
+}
