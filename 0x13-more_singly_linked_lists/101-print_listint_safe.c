e83598f3741ce42b694e1b7643faef9729c38d71
Rayanemadne <r.madoune@gmail.com>
2023-05-02 04:22:40 +0200

    script 101 update

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,27 +10,27 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *tmp = NULL;
-	const listint_t *testnode = NULL;
+	const listint_t *tmp_n = NULL;
+	const listint_t *l_n = NULL;
 	size_t counter = 0;
 	size_t new_n;
 
-	tmp = head;
-	while (tmp)
+	tmp_n = head;
+	while (tmp_n)
 	{
-		printf("[%p] %d\n", (void *)tmp, tmp->n);
+		printf("[%p] %d\n", (void *)tmp_n, tmp_n->n);
 		counter++;
-		tmp = tmp->next;
-		testnode = head;
+		tmp_n = tmp_n->next;
+		l_n = head;
 		new_n = 0;
 		while (new_n < counter)
 		{
-			if (tmp == testnode)
+			if (tmp_n == l_n)
 			{
-				printf("-> [%p] %d\n", (void *)tmp, tmp->n);
+				printf("-> [%p] %d\n", (void *)tmp_n, tmp_n->n);
 				return (counter);
 			}
-			testnode = testnode->next;
+			l_n = l_n->next;
 			new_n++;
 		}
 		if (!head)
