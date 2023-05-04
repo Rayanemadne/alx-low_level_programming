068afa2f7e3535344490fe7a9b5cd158beebb6b2
Rayanemadne <r.madoune@gmail.com>
2023-05-05 00:37:06 +0200

    script 2

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,6 +7,7 @@ int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 unsigned long int power(unsigned int base, unsigned int pow);
 void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 
 
 #endif
