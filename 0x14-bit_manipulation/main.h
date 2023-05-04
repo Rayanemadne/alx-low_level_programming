7dd3ea92bd809ba43e21707387e639e0d9728c0c
Rayanemadne <r.madoune@gmail.com>
2023-05-05 00:32:29 +0200

    script 1 fixing

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 unsigned long int power(unsigned int base, unsigned int pow);
 void print_binary(unsigned long int n);
