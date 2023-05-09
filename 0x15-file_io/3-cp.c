26a80c7800f36df5e7c127b7350b835be31770d2
Rayanemadne <r.madoune@gmail.com>
2023-05-09 02:00:24 +0200

    script 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,39 +1,11 @@
 #include "main.h"
 
+#define BUFSIZE 1024
+
 /*RAYANE - MAD*/
 
 /**
- * _exit - prints error messages
- *
- * @error: the exit number or file descriptor
- * @str: either file_in or file_out
- * @fd: file descriptor
- *
- * Return: 0
-*/
-int _exit(int error, char *str, int fd)
-{
-	switch (error)
-	{
-		case 97:
-			dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-			exit(error);
-		case 98:
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", str);
-			exit(error);
-		case 99:
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", str);
-			exit(error);
-		case 100:
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-			exit(error);
-		default:
-			return (0);
-	}
-}
-
-/**
- * main - create a copy of file
+ * main - copy file to another
  *
  * @argc: number of arguments
  * @argv: arguments
@@ -42,40 +14,44 @@ int _exit(int error, char *str, int fd)
 */
 int main(int argc, char *argv[])
 {
-	int file_in, file_out;
-	int read_stat, write_stat;
-	int close_in, close_out;
-	char buffer[1024];
+	int file_from, file_to, reading, cl_form, cl_to;
+	char buffer[BUFSIZE];
 
-	/*if arguments are not 3*/
 	if (argc != 3)
-		_exit(97, NULL, 0);
-	/*sets file descriptor for copy from file*/
-	file_in = open(argv[1], O_RDONLY);
-	if (file_in == -1)
-		_exit(98, argv[1], 0);
-	/*sets file descriptor for copy to file*/
-	file_out = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	if (file_out == -1)
-		_exit(99, argv[2], 0);
-	/*reads file_in as long as its not NULL*/
-	while ((read_stat = read(file_in, buffer, 1024)) != 0)
 	{
-		if (read_stat == -1)
-			_exit(98, argv[1], 0);
-		/*copy and write contents to file_out*/
-		write_stat = write(file_out, buffer, read_stat);
-		if (write_stat == -1)
-			_exit(99, argv[2], 0);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
-	/*close file_in*/
-	close_in = close(file_in);
-	if (close_in == -1)
-		_exit(100, NULL, file_in);
-	/*close file_out*/
-	close_out = close(file_out);
-	if (close_out == -1)
-		_exit(100, NULL, file_out);
-
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((reading = read(file_from, buffer, BUFSIZE)) > 0)
+	{
+		if (file_to < 0 || write(file_to, buffer, reading) != reading)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(file_from);
+			exit(99);
+		}
+	}
+	if (reading < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	cl_form = close(file_from);
+	cl_to = close(file_to);
+	if (cl_form < 0 || cl_to < 0)
+	{
+		if (cl_form < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		if (cl_to < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
 	return (0);
 }
